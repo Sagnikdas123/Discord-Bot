ba25624fc4c8ecd94b9b6ce8e8d2dabb9cb57e2a
Sagnikdas123 <75791539+Sagnikdas123@users.noreply.github.com>
2021-10-22 16:06:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// ODk5MjE4OTE5NjI4MzQ1NDE1.YWvk4Q.QKEkQLntXOEgLNddOUjYGHKR4d8
+
+const Discord = require('discord.js') 
+const bot = new Discord.Client();
+
+
+bot.login("ODk5MjE4OTE5NjI4MzQ1NDE1.YWvk4Q.QKEkQLntXOEgLNddOUjYGHKR4d8")
